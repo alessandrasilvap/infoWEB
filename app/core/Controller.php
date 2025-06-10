b5524841dbbd3c221bc28f8a001e0195ba03742c
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 10:27:59 -0300

    Create Controller.php

---
+++ app/core/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+class Controller {
+    public function view($view, $dados = []) {
+        extract($dados);
+        require_once __DIR__ . '/../views/' . $view . '.php';
+    }
+
+
+    public function model($model) {
+        require_once '../app/models/' . $model . '.php';
+        return new $model();
+    }
+}
+
+?>
