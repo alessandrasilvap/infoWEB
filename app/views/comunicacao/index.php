c3459112d527ea5e6b4b7f98c41e710de0656a6f
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 18:30:06 -0300

    ajeitando

--- app/views/comunicacao/index.php
+++ app/views/comunicacao/index.php
@@ -26,7 +26,7 @@
             </div>
         </div>
     </div>
-    <!-- Carrossel horizontal compacto -->
+    <!--Carrossel horizontal compacto-->
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="false" style="max-width: 700px; margin: auto;">
           <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -39,7 +39,7 @@
                   <img src="../public/imagens/comunicacao/imagem 3.png" class="d-block w-100" alt="Slide 3" style="height: 600px; object-fit: cover;">
                 </div>
           </div>
-          <!-- Setas laterais -->
+          <!--Setas laterais-->
           <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Anterior</span>
