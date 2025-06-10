2a81f659ac177145c86b696da462fbb0c7c70066
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 10:20:43 -0300

    Create DesafioController.php

---
+++ app/controllers/DesafioController.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../../DAO/quizDAO.php';
+require_once __DIR__ . '/../../DAO/conexao.php';
+
+class DesafioController extends Controller {
+    public function index() {
+        //Carrega perguntas do banco
+        $pdo = Conexao::getConexao();
+        $quizModel = new Quiz($pdo);
+        $perguntas = $quizModel->buscarPerguntasQuiz(7);
+      
+        //Carrega a view com as perguntas
+        $this->view('desafio/index', ['perguntas' => $perguntas]);
+    }
+}
+
+?>
