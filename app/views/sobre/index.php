22f6921c134454ed9bdae171b8445f77dc3fb76f
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 14:02:56 -0300

    Update index.php

--- app/views/sobre/index.php
+++ app/views/sobre/index.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário Responsivo</title>
-    <link rel="stylesheet" href="../home/index.css">
+    <link rel="stylesheet" href="../public/css/index.css">
 </head>
 <body>
     <div class="banner-azul">
-     <!--header-->
         <header>
             <div class="container">
                 <nav>
@@ -16,57 +15,39 @@
                         <a href="banenr.html">infoWEB</a>
                     </div>
                     <ul class="ul">
-                        <li> <a href="../home/index.html"> HOME</a></li>
-                        <li> <a href="../sobre nos/sobre.html"> sobre nós</a></li>
-                        <li> <a href="../pagina 3 Desafio/pagina 3.html"> Jogos</a></li>
-                     
+                        <li><a href="<?= BASE_URL ?>/home">Home</a></li>
+                        <li><a href="<?= BASE_URL ?>/sobre">Sobre nós</a></li>
+                        <li><a href="<?= BASE_URL ?>/desafio">Jogos</a></li>
                     </ul>
                     <div class="menu-icon">
-                        <img src="../home/index-img/menu.png" alt="" height="50">
+                        <img src="../public/imagens/home/menu.png" alt="" height="50">
                     </div>
                 </nav>
             </div>
         </header>
-          <script src="../home/menu-mob.js"></script>
-    </div>
-        <!-- end header-->
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
                 <ul class="nav">
                     <li class="nav-item">
-                        <a class="nav-link active" href="../pagina 1/pagina 1.html">Inicio</a>
+                        <a class="nav-link active" href="<?= BASE_URL ?>/home">Inicio</a>
                 </ul>
 
                 <!--Comentario Banner-->
-
-                <h2>
-                    Bem Vindo!
-                </h2>
-                <p>
-                    Ao site principal espero que tenha gostado.
-                </p><br>
-                <p>
-                    <a class="btn" href="#">ver detalhes »</a>
-                </p>
+                
                 <div>
-                    <br><br><p>Este site foi criado com o intuito de explicar questões básicas de informática essenciais para navegar na web, em sites especializados, informativos e até mesmo para usar redes sociais.
-                </p><br>
-                <img class="imagem"  src="sobre nós.JPG" width="800" height="400"/>
+                    <br><br><p>Este site foi criado com o intuito de explicar questões básicas de informática essenciais para navegar na web, em sites especializados, informativos e até mesmo para usar redes sociais.</p><br>
+                    <img class="imagem"  src="../public/imagens/sobre/sobre nós.JPG" width="800" height="400"/>
                 </div>
                 <nav class="pagination-lg">
                     <ul class="pagination">
                         <li class="page-item">
-                            <a class="page-link" href="../pagina 1/pagina 1.html">ínicio</a>
+                            <a class="page-link" href="<?= BASE_URL ?>/home">ínicio</a>
                     </ul>
                 </nav>
             </div>
         </div>
     </div>
-   
+    <script src="../public/js/menu-mob.js"></script>
 </body>
-
 </html>
-
-
-
