65619199b54ffd98b97c9b0ce54d6dfad5d6c5c8
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 13:58:33 -0300

    Update index.php

--- app/views/desafio/index.php
+++ app/views/desafio/index.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário Responsivo</title>
-    <link rel="stylesheet" href="../home/index.css">
+    <title>Desafios</title>
+    <link rel="stylesheet" href="../public/css/index.css">
 </head>
 <body>
     <div class="banner-azul">
-        <!--header-->
         <header>
             <div class="container">
                 <nav>
@@ -16,23 +15,17 @@
                         <a href="banenr.html">infoWEB</a>
                     </div>
                     <ul class="ul">
-                        <li> <a href="../home/index.html"> HOME</a></li>
-                        <li> <a href="../sobre nos/sobre.html"> sobre nós</a></li>
-                        <li> <a href="../pagina 3 Desafio/pagina 3.html"> Jogos</a></li>
-                     
+                        <li><a href="<?= BASE_URL ?>/home">Home</a></li>
+                        <li><a href="<?= BASE_URL ?>/sobre">Sobre nós</a></li>
+                        <li><a href="<?= BASE_URL ?>/desafio">Jogos</a></li>
                     </ul>
                     <div class="menu-icon">
-                        <img src="../home/index-img/menu.png" alt="" height="50">
+                        <img src="../public/imagens/home/menu.png" alt="" height="50">
                     </div>
                 </nav>
             </div>
         </header>
-          <script src="../home/menu-mob.js"></script>
-        <!-- end header-->
-    </div>
-         <!--main-->
-         
-        <!-- end main-->
+    </div>         
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
@@ -47,12 +40,7 @@
                         <a class="nav-link disabled" href="#"></a>
                     </li>
                 </ul>
-
-                <!-- Comentario Banner-->
-
-                </p>
                 <iframe style="max-width:100%" src="https://wordwall.net/pt/embed/ac9b785faa814f71b96e4a1fca8c12ea?themeId=2&templateId=10&fontStackId=0" width="500" height="580" frameborder="0" allowfullscreen></iframe>
-
                 <!--QUIZ-->
                 <h2 id="quiz">Quiz Eco Point</h2>
                 <p>Teste seu conhecimento sobre Reciclagem eletrônica! Você pode ser tornar um especialista no assunto. Participe do nosso Quiz Eco Point e descubra como você pode fazer a diferença!</p>
@@ -68,27 +56,22 @@
                 <script>
                     const questions = <?= json_encode($perguntas); ?>;
                 </script>
-                
                 <nav class="pagination-lg">
                     <ul class="pagination">
                         <li class="page-item">
-                            <a class="page-link" href="../pagina 1/pagina 1.html">1</a>
+                            <a class="page-link" href="<?= BASE_URL ?>/introducao">1</a>
                         </li>
                         <li class="page-item">
-                            <a class="page-link" href="../pagina 2/pagina 2.html">2</a>
+                            <a class="page-link" href="<?= BASE_URL ?>/dicas">2</a>
                         </li>
                         <li class="page-item">
-                            <a class="page-link" href="../pagina 3 Desafio/pagina 3.html">3</a>
+                            <a class="page-link" href="<?= BASE_URL ?>/desafio">3</a>
                         </li>
                     </ul>
                 </nav>
             </div>
         </div>
     </div>
-   
+    <script src="../public/js/menu-mob.js"></script>
 </body>
-
 </html>
-
-
-
