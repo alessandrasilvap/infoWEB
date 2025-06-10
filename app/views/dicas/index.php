68ea6414b16f6d375e7768f4d64aba1d59487984
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 14:36:22 -0300

    Update index.php

--- app/views/dicas/index.php
+++ app/views/dicas/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dicas</title>
-    <link rel="stylesheet" href="../ublic/css/index.css">
+    <link rel="stylesheet" href="../public/css/index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
@@ -100,6 +100,38 @@
                     <button id="Mais-button">Mais</button>
                 </div>
             </div>
+    <!--footer-->
+    <footer>
+         <div class="footer-container">
+            <div class="footer-content">
+                <h3>Contact Us</h3>
+                <p> Email:Info@exemplo.com</p>
+                <p>Phone:121 56556 5655556</p>
+                <p>Adress</p>
+            </div>
+            <div class="footer-content">
+                <h3>Desenvolvedores</h3>
+                <ul class="list">
+                    <li><a href="">Alessandra Cristina da Silva Pereira</a></li>
+                    <li><a href="">Bryan Caristiati Costa</a></li>
+                    <li><a href="">Eric Luiz Xavier de Araujo</a></li>
+                    <li><a href="">Daniel Jesus Dias Alves</a></li>
+                    <li><a href="">Gabriel Araújo de Oliveira</a></li>
+                </ul>
+            </div>
+            <div class="footer-content">
+                <h3>Fallow Us</h3>
+                <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                <li><a href=""><i class="fab fa-Twitter"></i></a></li>
+                <li><a href=""><i class="fab fa-Instagram"></i></a></li>
+                <li><a href=""><i class="fab fa-Linkedin"></i></a></li>
+            </div>
+         </div>
+         <div class="btn-bar">
+            <p>&copy;2025 your compan . All rights reserved </p>
+         </div>
+     </footer>
+     <!--end-footer-->
     <script src="../public/js/card.js"></script>
     <script src="../public/js/menu-mob.js"></script>
 </body>
