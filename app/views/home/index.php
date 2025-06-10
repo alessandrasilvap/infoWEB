aefba7d50a6a549efd8c4913d2d4af1a1b188734
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 10:30:32 -0300

    Create index.php

---
+++ app/views/home/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário Responsivo</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="banner-azul">
+        <!--header-->
+        <header>
+            <div class="container">
+                <nav>
+                    <div class="logo">
+                        <a href="banenr.html">infoWEB</a>
+                    </div>
+                    <ul class="ul">
+                        <li> <a href="../home/index.html"> HOME</a></li>
+                        <li> <a href="../sobre nos/sobre.html"> sobre nós</a></li>
+                        <li> <a href="../pagina 3 Desafio/pagina 3.html">Jogos</a></li>
+                    </ul>
+                    <div class="menu-icon">
+                        <img src="index-img/menu.png" alt="" height="50">
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <script src="menu-mob.js"></script>
+        <!-- end header-->
+         <!--main-->
+        <main>
+            <div class="container">
+                <div class="main-text">
+                    <h1>Navegando e aprendendo com segurança</h1>
+                    <div>
+                        <img src="index-img/verificado.png" alt="Verificado" height="50">
+                    </div>
+                </div>
+                   <!--main-img-->
+            <div class="main-img">
+                <img src="index-img/note.banner.webp" alt="banner-comput">
+            </div>
+            <!--end main-img-->
+            </div>
+        </main>
+        <!-- end main-->
+    </div>
+<!--section-->
+
+    <section id="sobre">
+        <h2>O que é o ‘Navegando com Segurança’?</h2>
+        <p>O ‘Navegando com Segurança’ é um projeto educativo criado para ajudar as pessoas a se protegerem na internet. Aqui você encontrará conteúdos interativos, dicas de segurança e um quiz para testar seus conhecimentos.</p>
+        
+        <div class="beneficios">
+            <a href="../pagina 2/pagina 2.html" class="beneficio">
+                <img src="index-img/proteja-se.png"Segurança">
+                <p>Aprenda a evitar golpes online</p>
+            </a>
+            <a href="../pagina 2/" class="beneficio">
+                <img src="index-img/conhecimento.png" alt="Quiz">
+                <p>Teste seus conhecimentos</p>
+            </a>
+            <a href="../pagina 1/pagina 1.html" class="beneficio">
+                <img src="index-img/informatica.png" alt="Aulas">
+                <p>Conceitos Basicos de Informática </p>
+            </a>
+        </div>
+    </section>
+<!--end-section-->
+     <!--footer-->
+     <footer>
+     <div class="footer-container">
+        <div class="footer-content">
+            <h3>Contact Us</h3>
+            <p> Email:Info@exemplo.com</p>
+            <p>Phone:121 56556 5655556</p>
+            <p>Adress</p>
+        </div>
+        <div class="footer-content">
+            <h3>Quick link</h3>
+            <ul class="list">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Products</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+        <div class="footer-content">
+            <h3>Fallow Us</h3>
+            <li><a href=""><i class="fab fa-facebook"></i></a></li>
+            <li><a href=""><i class="fab fa-Twitter"></i></a></li>
+            <li><a href=""><i class="fab fa-Instagram"></i></a></li>
+            <li><a href=""><i class="fab fa-Linkedin"></i></a></li>
+        </div>
+     </div>
+     <div class="btn-bar">
+        <p>&copy;2025 your compan . All rights reserved </p>
+     </div>
+    
+
+     </footer>
+
+     <!--end-footer-->
+
+
+</body>
+
+</html>
+
+
