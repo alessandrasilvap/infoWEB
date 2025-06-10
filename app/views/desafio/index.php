13d77c30ba1ff1f697aff39e590d592b3144e9ce
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-10 20:09:04 -0300

    Atualizações

--- app/views/desafio/index.php
+++ app/views/desafio/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafios</title>
     <link rel="stylesheet" href="../public/css/index.css">
+    <link rel="stylesheet" href="../public/css/quiz.css">
 </head>
 <body>
     <div class="banner-azul">
